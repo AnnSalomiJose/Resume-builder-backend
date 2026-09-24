ee9d23df596dda2fa48a16022b2b9efddbaa33a9
AnnSalomiJose <annsalomijose@gmail.com>
2026-09-24 12:39:34 +0530

    backend server completed

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonserver = require("json-server");
+
+const server = jsonserver.create();
+
+const router = jsonserver.router("db.json");
+
+const middlewares = jsonserver.defaults();
+
+const port =  3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+    console.log('json server is runnig on port",port)')
+})
